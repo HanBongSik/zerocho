14d41ed3f736b44d804b17e216805cd52bd801a8
bongsikhan <bongsikhan@gmail.com>
2020-10-14 15:19:30 +0900

    aaa

--- ch1/front/layouts/admin.vue
+++ ch1/front/layouts/admin.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <nav> 
-            <nuxt-link to="/">Home</nuxt-link>
+            <nuxt-link to="/">Home1</nuxt-link>
             <nuxt-link to="/profile">Profile</nuxt-link>
             <nuxt-link to="/signup">Signup</nuxt-link>
             <nuxt-link to="/admin">Admin</nuxt-link>
